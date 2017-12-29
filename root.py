e8bb55ae140522d0014ff9de28eb8bee455c33b0
wu <git@segmentum.solar>
2017-12-29 17:57:16 +0100

    codecleaning and notifivations

--- root.py
+++ root.py
@@ -6,10 +6,13 @@ mosquitto_sub -v -h localhost -t "world/fff/all/clients"
 '''
 
 
+
+
 ## libs
 import sys
 import requests
 import paho.mqtt.publish as mqttpublish
+import notify2
 
 
 ## checks
@@ -17,7 +20,8 @@ if sys.version_info[0] != 3:
     print("This script requires Python 3")
     exit()
 
-
+# init notifications
+notify2.init('freifunkapi2mqtt')
 
 
 ## helpfunctions
@@ -27,12 +31,11 @@ def is_int_able(intablestring):
     '''
     trys to convert a str to number via int()
     '''
-    try:
+    try:    
         int(intablestring)
         return True
     except ValueError:
         return False
-
 
 ## script
 
@@ -40,46 +43,30 @@ def is_int_able(intablestring):
 resp_userinfo = requests.get("https://monitoring.freifunk-franken.de/api/routers_by_nickname/wu")
 
 if resp_userinfo.ok:
-
     # generate router tuple
-
     nodes = resp_userinfo.json()['nodes']
-
     node_ids = []
-
+    
     for node in nodes:
-        #print (node['name'])
-        #print (node['oid'])
+        #dict['name''oid']
         node_ids.append(node['oid'])
-
-
-    print(node_ids)
-
-    # gen clientcount
 
     clients = []
 
     for node_id in node_ids:
         #print(node_id)
-        resp_nodeinfo = requests.get(
-            "https://monitoring.freifunk-franken.de/routers/{}?json".format(node_id)
-            )
+        resp_nodeinfo = requests.get("https://monitoring.freifunk-franken.de/routers/{}?json".format(node_id))
         if resp_nodeinfo.ok:
-            #print(resp_nodeinfo.json()["clients"])
-            #print(resp_nodeinfo.json()["status"])
-            #print(resp_nodeinfo.json()["hostname"])
-            #print(resp_nodeinfo.json()["position_comment"])
+            #dict:["clients""status""hostname""position_comment"]
             clients.append(int(resp_nodeinfo.json()["clients"]))
-            #                   "world/fff/clients/nodenumber"
+            #"world/fff/clients/nodenumber"
             mqttpublish.single("world/fff/{}/clients".format(node_id), int(resp_nodeinfo.json()["clients"]), hostname="localhost")
         else:
             print("nodeinfo error")
-        
-    print(clients)
-    print(sum(clients))
 
     mqttpublish.single("world/fff/all/clients", sum(clients), hostname="localhost")
-    
+    n = notify2.Notification('current freifunk clients', str(sum(clients)))
+    n.show()
 else:
     print('userinfo error')
 
