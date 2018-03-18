1f396552a603d88fb016ec6008e8f7a339471702
WU <git@segmentum.solar>
2018-03-19 00:23:56 +0100

    logging

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # py_freifunk_api
 
 ## test
-
+```
 sudo apt-get install mosquitto mosquitto-clients
 sudo apt-get install python3-pip
-
+```
 
 ## mqtt test
+```
 mosquitto_sub -v -h localhost -t '#'
 mosquitto_sub -v -h localhost -t "world/fff/+/+/clients" -T "world/fff/+/all/#"
 mosquitto_sub -v -h localhost -t "world/fff/+/all/clients"
-
+```
--- freifunkapi2mqtt.py
+++ freifunkapi2mqtt.py
@@ -1,23 +1,52 @@
-'''
+"""
 put help text here
-'''
+"""
 
-## checks
+# checks
 import sys
 
 if sys.version_info[0] != 3:
     print("This script requires Python 3")
     exit()
 
-## libs
+# libs
 
 import requests
 import paho.mqtt.publish as mqttpublish
 import notify2
 import time
 import threading
+import logging
 
-## config
+# logging
+
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+# file logging
+fh = logging.FileHandler('spam.log')
+fh.setLevel(logging.ERROR)
+# logger shell output
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+# set formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+# 'application' code
+logger.debug('debug message')
+logger.info('info message')
+logger.warn('warn message')
+logger.error('error message')
+logger.critical('critical message')
+
+
+# config
+logger.info('reading config')
 
 FREIFUNKFRANKEN_USER_NODE_QUERRY_URL = "https://monitoring.freifunk-franken.de/api/routers_by_nickname/{}"
 FREIFUNKFRANKEN_NODE_QUERRY_URL = "https://monitoring.freifunk-franken.de/routers/{}?json"
@@ -27,10 +56,11 @@ MQTT_HOST = 'localhost'
 NOTIFICATIONS_TITLE = 'freifunkapi2mqtt'
 NOTIFICATIONS_FLUFF = 'current freifunk clients for {}'
 
-PULBISHING_CYCLE = 300  # every 5 minutes
+PULBISHING_CYCLE = 10  # every 5 minutes
 
-## objects
 
+# objects
+logger.info('defining config')
 
 class Node(object):
     """
@@ -55,6 +85,7 @@ class Node(object):
         self.status = None
         self.sys_uptime = None
         self.clients = None
+        logger.debug("Node init " + str(self.name))
 
     def extend_with_node_api_response(self, node_api_response):
         node_api_response_json = node_api_response.json()
@@ -69,18 +100,21 @@ class Node(object):
         self.status = node_api_response_json['status']
         self.sys_uptime = node_api_response_json['sys_uptime']
         self.clients = node_api_response_json['clients']
+        logger.debug("Node extended with api response " + str(self.name))
 
     def is_online(self):
-        return (self.status == 'online')
+        logger.debug("Node.is_online " + str(self.status == 'online'))
+        return self.status == 'online'
 
     def has_clients(self):
-        return (self.clients > 0)
+        logger.debug("Node.has " + str(self.clients > 0))
+        return self.clients > 0
 
 
 class FreifunkClient(object):
-    '''
-    Client for FFF Node requestes and publishing 
-    '''
+    """
+    Client for FFF Node requestes and publishing
+    """
 
     def __init__(self,
                  username,
@@ -99,30 +133,33 @@ class FreifunkClient(object):
         self.client_count = 0
         self.notifications_status = False
         self.mqtt_status = False
+        logger.debug("FreifunkClient init " + str(self.username))
 
     def init_notifications(self,
                            notifications_title=NOTIFICATIONS_TITLE,
                            notifications_fluff=NOTIFICATIONS_FLUFF):
-        '''
+        """
         set the notifications variables and inititates the notifications module
-        '''
+        """
         self.notifications_title = notifications_title
         self.notifications_fluff = notifications_fluff
         notify2.init(self.notifications_title)
         self.notifications_status = True
+        logger.debug("FreifunkClient init_notifications " + str(self.username))
 
     def init_mqtt(self, mqtt_host=MQTT_HOST, mqtt_path=MQTT_PATH):
-        '''
+        """
         set the mqtt variables
-        '''
+        """
         self.mqtt_host = MQTT_HOST
         self.mqtt_path = mqtt_path
         self.mqtt_status = True
+        logger.debug("FreifunkClient init_mqtt " + str(self.username))
 
     def fetch_user_node_data(self):
-        '''
+        """
         make api call and populate self.nodes with Nodes
-        '''
+        """
         user_node_api_response = requests.get(
             self.api_url_user_nodes.format(self.username))
         user_node_api_response_json = user_node_api_response.json()
@@ -136,13 +173,14 @@ class FreifunkClient(object):
             self.nodes.append(node)
 
         self.node_count = len(self.nodes)
+        logger.debug("FreifunkClient fetch_user_node_data " + str(self.node_count))
 
     def publish_clients(self):
-        '''
+        """
         publishes client data to the enabled destinations
         * mqtt
         * notifications
-        '''
+        """
         if self.mqtt_status:
             mqttpublish.single(
                 self.mqtt_path.format(self.username, 'all', 'clients'),
@@ -162,10 +200,12 @@ class FreifunkClient(object):
             for i in range(0, len(self.nodes)):
                 pass
 
+        logger.debug("FreifunkClient publish_clients " + str(self.client_count))
+
     def update_nodes(self):
-        '''
+        """
         cycles throug nodes and overides current data with requested data
-        '''
+        """
         for i in range(0, len(self.nodes)):
             node_api_response = requests.get(
                 self.api_url_nodes.format(self.nodes[i].oid))
@@ -174,29 +214,34 @@ class FreifunkClient(object):
         self.client_count = sum(
             node.clients for node in self.nodes if node.clients is not None)
 
+        logger.debug("FreifunkClient update_nodes " + str(self.self.nodes))
+
     def _continuous_publishing(self):
-        '''
+        """
         just triggers an update_nodes and then a publish_clients
         intended for threaded use
-        '''
+        """
         while True:
             try:
                 self.update_nodes()
                 self.publish_clients()
             except:
                 pass
+            logger.debug("FreifunkClient _continuous_publishing cycle")
             time.sleep(self.pulbishing_cycle)
 
-    def continuous_publishing_threaded(self):
-        '''
-        strats a thread with _continuous_publishing
-        '''
-        threading.Thread(target=self._continuous_publishing).start()
 
+    def continuous_publishing_threaded(self):
+        """
+        strats a thread with _continuous_publishing
+        """
+        threading.Thread(target=self._continuous_publishing).start()
+        logger.debug("FreifunkClient continuous_publishing_threaded started")
 
 if __name__ == "__main__":
 
-    users = ['wu', 'wuex','backspace']
+    logger.info("starting __main__")
+    users = ['wu', 'wuex', 'backspace']
 
     # create clients ready for threading
     fffcl = []
